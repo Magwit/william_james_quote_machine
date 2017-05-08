4fcd6abc5d9e6a6a29ae6eb4248ea50084541d94
Magwit <magnushakon@hotmail.com>
2017-05-08 19:46:30 +0300

    add font and create palette

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
     console.log('Hiya ! jQuery');
-})
+
+    // Variables for dom elements
+
+    var $quotezone = $('.quote-zone');
+  //   var $btn = 
+    
+    
+    // Manipulate dom
+    
+  //   $quotezone.css('color', 'gainsboro');
+
+    // Button on click function.
+
+
+
+
+
+});
 
 
