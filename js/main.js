dcf2ab8c004ea6adc5bc10618d3fa266e9278552
Magwit <magnushakon@hotmail.com>
2017-05-18 18:35:59 +0200

    create basic quote function w ajax

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
 	console.log('Hiya ! jQuery');
 
+	var url = 'js/quotedata.json';
+
 	// ### Variables for dom elements ###
 
-	//  var $quotezone = $('.quote-zone');
 	var $openBtn = $('.open-btn');
 	var $modalClose = $('.modal-close');
+	var $quotestring = $('.modal-content > h5') // .prepend(currentQuote);
+	var $modalBackground = $('.modal-background');
 
 	// ### Modal DOM manipulation ###
 
@@ -13,37 +16,50 @@ $(document).ready(function () {
 		modal.style.visibility = "visible"
 		modal.style.opacity = 1
 		document.body.style.overflow = "hidden";
+		
+		// Ajax call to quote jason
+		$.ajax({
+			type:"GET",
+			url:url,
+			success: function(data) {
+
+				console.log(typeof(data));
+				console.log(data[0].quotes[8]);
+				// specify the array for quotes  
+				var quoteArr = data[0].quotes;
+
+				function Shuffle(o) {
+        for (
+          var j, x, i = o.length;
+          i;
+          (j = parseInt(Math.random() * i)), (x = o[--i]), (o[i] = o[j]), (o[
+            j
+          ] = x)
+        );
+        return o;
+      }
+
+			Shuffle(quoteArr);
+			
+			var chosenQuote = quoteArr[0];
+
+			$($quotestring).html(chosenQuote);
+
+			}
+		})
 	})
 
-	$($modalClose).on('click', function () {
+	$($modalClose, $modalBackground).on('click', function () {
 		modal.style.visibility = "hidden"
 		modal.style.opacity = 0
 		document.body.removeAttribute("style");
 	})
 
-
 	console.log('end of jQuery');
 
 });
 
-// Ugly hack below since there is a scope problem if
+
 // Functions come up as undefined if placed in the document ready function. 
 // http://stackoverflow.com/questions/1055767/why-can-i-not-define-functions-in-jquerys-document-ready
 // or you can use $('#element').on('click', function() {}) within `$(document).ready().
-
-/*
-	function openModal(){
-		var modal = document.getElementById("modal");
-		modal.style.visibility = "visible"
-		modal.style.opacity = 1
-		document.body.style.overflow = "hidden";
-	}
-*/
-/*
-function closeModal() {
-	var modal = document.getElementById("modal");
-	modal.style.visibility = "hidden"
-	modal.style.opacity = 0
-	document.body.removeAttribute("style");
-}
-*/
