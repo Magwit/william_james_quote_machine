351ea48de8c2c9143cd8ee9f9c10a1a8c45215af
Magwit <magnushakon@hotmail.com>
2017-05-19 10:19:43 +0200

    create Ajax call on page load instead of on click event

--- js/main.js
+++ js/main.js
@@ -10,43 +10,44 @@ $(document).ready(function () {
 	var $quotestring = $('.modal-content > h5') // .prepend(currentQuote);
 	var $modalBackground = $('.modal-background');
 
+	// ### Ajax call to fetch a quote ###
+
+	$.ajax({
+		type: "GET",
+		url: url,
+		success: function (data) {
+
+			console.log(typeof (data));
+			console.log(data[0].quotes[8]);
+			// specify the array for quotes  
+			var quoteArr = data[0].quotes;
+
+			function Shuffle(o) {
+				for (
+					var j, x, i = o.length; i;
+					(j = parseInt(Math.random() * i)), (x = o[--i]), (o[i] = o[j]), (o[
+						j
+					] = x)
+				);
+				return o;
+			}
+
+			Shuffle(quoteArr);
+
+			var chosenQuote = quoteArr[0];
+
+			$($quotestring).html(chosenQuote);
+
+		}
+	})
+
 	// ### Modal DOM manipulation ###
 
 	$($openBtn).on('click', function () {
 		modal.style.visibility = "visible"
 		modal.style.opacity = 1
 		document.body.style.overflow = "hidden";
-		
-		// Ajax call to quote jason
-		$.ajax({
-			type:"GET",
-			url:url,
-			success: function(data) {
 
-				console.log(typeof(data));
-				console.log(data[0].quotes[8]);
-				// specify the array for quotes  
-				var quoteArr = data[0].quotes;
-
-				function Shuffle(o) {
-        for (
-          var j, x, i = o.length;
-          i;
-          (j = parseInt(Math.random() * i)), (x = o[--i]), (o[i] = o[j]), (o[
-            j
-          ] = x)
-        );
-        return o;
-      }
-
-			Shuffle(quoteArr);
-			
-			var chosenQuote = quoteArr[0];
-
-			$($quotestring).html(chosenQuote);
-
-			}
-		})
 	})
 
 	$($modalClose, $modalBackground).on('click', function () {
